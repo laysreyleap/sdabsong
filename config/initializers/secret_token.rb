4f8455cbbcd022f382286818853edd52b4ce6aab
Lay Sreyleap <lay.sreyleap@gmail.com>
2013-01-09 17:52:20 +0700

    start rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sdabsong::Application.config.secret_token = '8ff094428cf5e63ac5784b240849a76ef86af42b6b87f2f8d0de837a438884ad60b33a4b61de6bc052714536835e6fe7b382444074e9c65f6e067d373f87c4fa'
